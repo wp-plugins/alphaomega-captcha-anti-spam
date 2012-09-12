fc58b4c084432cf28916ead0f66667d61ed39615
alphaomegaplugins <alphaomegaplugins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-12 14:51:03 +0000

    version number increase
    git-svn-id: https://plugins.svn.wordpress.org/alphaomega-captcha-anti-spam/trunk@598038 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- alphaomega-captcha-and-anti-spam.php
+++ alphaomega-captcha-and-anti-spam.php
@@ -3,7 +3,7 @@
 Plugin Name: AlphaOmega Captcha & Anti-Spam Filter
 Plugin URI: http://alphaomegaplugins.com
 Description: Creates Visually Appealing Captcha Images and Quizzes to Enhance and Secure your WordPress Comment/Log In Forms. Also includes Anti-Spam Protection.
-Version: 1.3
+Version: 1.4
 Author: AlphaOmega Plugins
 Author URI: http://alphaomegaplugins.com
 License: GPL2

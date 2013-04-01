6859c78e00cdc23e33842b1faa9fd0203349d08c
alphaomegaplugins <alphaomegaplugins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-01 17:45:35 +0000

    telios
    git-svn-id: https://plugins.svn.wordpress.org/alphaomega-captcha-anti-spam/trunk@690210 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- addenda/delimit.php
+++ addenda/delimit.php
@@ -1,6 +1,1 @@
-<?php
 
-$aokatastasi = '1.7';
-$aokolonaproto = array('English','Japanese','Greek','Korean','Chinese');
-
-?>
--- addenda/retop.php
+++ addenda/retop.php
@@ -1,10 +1,1 @@
-<?php
 
-if ( get_option( 'aostichia' ) !== FALSE ) {
-
-	$michanionoma = ' AlphaOmega Captcha ';
-	$aostichiaoa = get_option( 'aostichia' );
-
-} 
-
-?>
